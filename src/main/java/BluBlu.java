7bac29bb96ca608236b3c72e19c6b1b42866ecbd
stav alfi <stav.alfi@nice.com>
2017-09-29 22:06:15 +0300

    4

--- src/main/java/BluBlu.java
+++ src/main/java/BluBlu.java
@@ -1,5 +1,8 @@
 public class BluBlu {
-    public static void main(String[] args) {
-        System.out.println("sss");
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("ccccc");
     }
 }
