ebf5c6d506905e8289cb166d1a19a4467745951f
stav alfi <stav.alfi@nice.com>
2017-10-28 23:22:28 +0300

    1

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,18 +1,23 @@
 package hello;
 
-import lombok.SneakyThrows;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
+@SpringBootApplication
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    @SneakyThrows
-    public String index() {
-        String hostname = InetAddress.getLocalHost().getHostName();
-        return "Hostname: "+hostname+ "! Greetings from Spring Boot!";
+    public String home() throws UnknownHostException {
+        return "Hostname: "+InetAddress.getLocalHost().getHostName()+", Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloController.class, args);
     }
 
 }
