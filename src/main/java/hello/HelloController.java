8e7abc36c58d21b45d30c600685063548b45b3db
stav alfi <stav.alfi@nice.com>
2017-10-28 17:43:26 +0300

    1

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,18 @@
 package hello;
 
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.net.InetAddress;
 
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
+    @SneakyThrows
     public String index() {
-        return "Greetings from Spring Boot!";
+        String hostname = InetAddress.getLocalHost().getHostName();
+        return "Hostname: "+hostname+ "!      Greetings from Spring Boot!";
     }
 
 }
