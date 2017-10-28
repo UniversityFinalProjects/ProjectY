4efde6453b6fcc86077952a639e2ba5732b628d4
stavalfi <stavalfi@gmail.com>
2017-10-29 00:43:08 +0300

    1 (#4)

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,16 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
+
 
 @SpringBootApplication
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
-    public String home() throws UnknownHostException {
-        return "Hello Docker World";
+    public String home(){
+
+        return getMachineInfo() + ", Hello Docker World";
+    }
+
+    private static String getMachineInfo(){
+        Map<String, String> env = System.getenv();
+        if (env.containsKey("COMPUTERNAME"))
+            return env.get("COMPUTERNAME");
+        else if (env.containsKey("HOSTNAME"))
+            return env.get("HOSTNAME");
+        else
+            return "Unknown Computer";
     }
 
     public static void main(String[] args) {
